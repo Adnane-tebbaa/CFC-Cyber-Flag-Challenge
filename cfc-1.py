b00535c12aa8396453ecea5ef8556a7d9dd8ca87
Techni-world <46726810+Adnane-tebbaa@users.noreply.github.com>
2019-03-24 16:54:47 +0100

    Add files via upload

---
+++ cfc-1.py
@@ -0,0 +1,24 @@
+import sys
+import os
+import sys
+import time
+from colorama import init
+init(strip=not sys.stdout.isatty()) 
+from termcolor import cprint 
+from pyfiglet import figlet_format
+print " "
+cprint(figlet_format(' CFC- 1', font='starwars'),
+       'yellow', attrs=['bold']) 
+print   "\033[0;33m - Cyber Flag Challenge - "
+print " "
+print " www.techni-world.com "
+print " coded by adnane tebbaa (hacktheplanet)"
+print " "
+print " CFC-1 /codename:ASCII_HEX "
+print " "
+print " \033[31m - Target Encryption: 66 6C 61 67 20 32 30 31 39 "
+
+
+
+
+
